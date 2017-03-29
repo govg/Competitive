7096591bf3ad09cc51b4d8fbc9d836fc4ef1d27e
Govind Gopakumar <govind.93@gmail.com>
2017-03-30 00:47:26 +0530

    WIP Template

--- template.cpp
+++ template.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 #include<cmath>
 
+#include<vector>
+#include<set>
+#include<queue>
+#include<stack>
+#include<map>
+#include<string>
+
+#include<algorithm>
+
 using namespace std;
 
 int main()
